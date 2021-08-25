b8f6db1f6ca01532f6754011b026e405d36c72e7
DesignBun <89509272+DesignBun@users.noreply.github.com>
2021-08-25 10:01:46 +0300

    Add files via upload

---
+++ DB (1).user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         DB
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       You
+// @match        https://designbundles.net/search?search=*
+// @icon         https://www.google.com/s2/favicons?domain=designbundles.net
+// @grant        none
+// ==/UserScript==
+
+let pause = 5000; // Пауза между кликами
+let T = pause; // Первая пауза
+setTimeout(() => {
+    //alert("Hello");
+    let count = prompt('Сколько Кликов делать?', 1);
+    let now = 0;
+    let card = document.querySelectorAll('div.search-body__products-row > div');
+    let i = 0 ;
+    function myLoop() {
+        setTimeout(() => {
+            let fav = card[i].querySelector('.product-box__social');
+            let favStyle = window.getComputedStyle(fav);
+            let cart = card[i].querySelector('.product-box__add-cart');
+            let cartStyle = window.getComputedStyle(cart);
+            console.log(favStyle.getPropertyValue('background-color') + cartStyle.getPropertyValue('background-color') );
+            if ((favStyle.getPropertyValue('background-color') == 'rgb(255, 255, 255)') & ( cartStyle.getPropertyValue('background-color') == 'rgb(76, 197, 251)' ) ) {
+                let pin = card[i].querySelector('.-pinterest');
+                pin.click();
+                fav.click();
+                now++;
+                console.log(`Выполнил кликов ${now}`);
+                //debugger;
+                T = pause;
+                if (now >= count) { i = 9999; };
+            } else {
+                T = 100;
+                //alert("-");
+            }
+            i++;
+            if (i < 36) {
+                myLoop();
+            } else { alert(`Выполнил кликов ${now}`); }
+
+        }, Math.random() * T + T);
+    }
+    myLoop();
+}, Math.random() * 2000 + 2000);
